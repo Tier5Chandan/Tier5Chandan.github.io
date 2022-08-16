a812c17bd1f791c2ced4fc5ff6705e96d48b416f
= <=>
2022-08-16 22:20:13 +0530

    Change css style

--- js/main.js
+++ js/main.js
@@ -40,14 +40,17 @@
 $("#programFilter .nav-item .nav-link").click((e) => {
     if (e.target.id !== "all_class") {
         $("#class-all td a").removeClass("active");
-        $("#class-all table td").css("background","");
+        $("#class-all table td").css("background", "");
+        $("#class-all table td").css("transform", "");
         $("." + e.target.id).css("background", "var(--primary)");
-        $("."+ e.target.id + " a").addClass("active");
+        $("." + e.target.id + " a").addClass("active");
+        $("." + e.target.id).css("transform", "scale(1.1)");
     }
     if (e.target.id == "all_class") {
         $("#class-all td a").removeClass("active");
         $("#class-all table td").css("background", "");
-        $("#class-all table td").css("color","#343a40");
+        $("#class-all table td").css("color", "#343a40");
+        $("#class-all table td").css("transform", "");
     }
 });
 
